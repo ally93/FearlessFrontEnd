aca8d227c89d21862593cae9302cd18fa0b63502
Ally <allygoose93@gmail.com>
2022-10-11 15:14:22 -0700

    firstFetch and Data to DOM done

---
+++ README.md
@@ -0,0 +1,5 @@
+-in ghi/js/app.js get the response
+-await needs async in the function
+
+-You can use a CSS query selector to get a reference to that tag. Then, you set the innerHTML property of the element to set the name of the conference to the content of that element.
+    const nameTag = document.querySelector('.card-title');
--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/';
+    
+    try{
+        const response = await fetch(url);
 
+        if(!response.ok){
+            console.log('Bad response buddy!');
+        } else{
+            const data = await response.json();
+
+            const conference = data.conferences[0];
+            const nameTag = document.querySelector('.card-title');
+            nameTag.innerHTML = conference.name;
+
+            
+
+            const detailUrl = `http://localhost:8000${conference.href}`;
+            const detailResponse = await fetch(detailUrl);
+            if (detailResponse.ok) {
+                const details = await detailResponse.json();
+                const description = details.conference.description;
+                const descriptionTag = document.querySelector('.card-text');
+                descriptionTag.innerHTML = description
+                
+            }
+
+            
+        }
+    } catch (e) {
+        console.log('Got an error!');
+    }
 
 });
