79dda1ca10fc3d5f9f128482ea4817b2f1a0b387
Ally <allygoose93@gmail.com>
2022-10-12 15:22:36 -0700

    creating a location done, added a name to input tags in new-location.html

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -31,8 +31,6 @@ window.addEventListener('DOMContentLoaded', async () => {
             selectTag.appendChild(option);
         }
 
-
-
       }
     } catch (e) {
         console.error('Got an error!');
@@ -41,5 +39,31 @@ window.addEventListener('DOMContentLoaded', async () => {
         const somethingWrong = document.querySelector("#error");
         somethingWrong.innerHTML = newHTML;
     }
+
+    // locationFormSubmit
+
+    const formTag = document.getElementById('create-location-form');
+    formTag.addEventListener('submit',async event => {
+        event.preventDefault();
+
+        const formData = new FormData(formTag);
+        const json = JSON.stringify(Object.fromEntries(formData));
+        
+        const locationUrl = 'http://localhost:8000/api/locations/';
+        const fetchConfig = {
+        method: "post",
+        body: json,
+        headers: {
+            'Content-Type': 'application/json',
+            },
+        };
+        const response = await fetch(locationUrl, fetchConfig);
+        if (response.ok) {
+            formTag.reset();
+            const newLocation = await response.json();
+            
+        }
+        
+    });
   
-  });
+});
